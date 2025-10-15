ceb865d706627e1b08572a8a68999aba7fbfb884
KreKer_ <platon.bob@gmail.com>
2025-10-16 00:59:55 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -3,19 +3,8 @@ import {MiniMaple} from './miniMaple.js';
 document.addEventListener('DOMContentLoaded', setup)
 
 function setup() {
-    document.getElementById('demoButton').onclick = addSomething;
     document.getElementById('diffButton').onclick = calculateDerivative;
 }
-
-function addSomething(){
-    const someDummyDiv = document.createElement('div');
-    someDummyDiv.classList.add('generated');
-    const count = document.getElementsByClassName('generated').length;
-    someDummyDiv.innerHTML = `I was created by JS! There are already ${count} of my friends!`;
-    const container = document.getElementById('container');
-    container.appendChild(someDummyDiv);
-}
-
 
 function calculateDerivative() {
     const func = document.getElementById('functionInput').value;
